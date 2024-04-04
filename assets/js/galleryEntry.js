7dd21f0e7d78c6e2b0bd9f6635bf76e41aa3f4f4
Kevin <kevin.barr88@gmail.com>
2024-04-04 16:42:11 +0100

    Centered Header, fixed image positioning, stretch + border

--- assets/js/galleryEntry.js
+++ assets/js/galleryEntry.js
@@ -1,11 +1,6 @@
 class GalleryEntry {
-  constructor(name, searchTerm, imageURL, imageWidth, imageHeight) {
+  constructor(name, imageURL) {
     this.name = name;
-    this.searchTerm = searchTerm === "" ? this.name : searchTerm;
     this.imageURL = imageURL;
-    this.imageWidth = imageWidth;
-    this.imageHeight = imageHeight;
-
-    getImageFromRemote(this);
   }
 }
--- assets/js/unsplashHandler.js
+++ assets/js/unsplashHandler.js
@@ -1,7 +1,10 @@
+/*
 async function getImageFromRemote() {
   console.log(`Fetching!`);
   const response = await fetch("https://source.unsplash.com/random");
   console.log(`Response: ${response}`);
 
-  imgElement.src = window.URL.createObjectURL(await response.blob());
+  const imgBlob = await response.blob();
+  imgElement.src = window.URL.createObjectURL(imgBlob);
 }
+*/
